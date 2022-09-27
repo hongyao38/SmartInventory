e39bea35d4e9d58c426437ee2140bfa8d2f3da69
geraldng1407 <g-e-r-a-l-d@hotmail.com>
2022-09-27 09:22:36 +0800

    Added findByEmailIgnoreCase function

--- src/main/java/com/smartinventory/User/UserRepository.java
+++ src/main/java/com/smartinventory/User/UserRepository.java
@@ -9,4 +9,8 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository <User, Long> {
     
     Optional<User> findByUsername(String username);
+
+	User findByEmailIgnoreCase(String emailId);
+        
+    
 }
