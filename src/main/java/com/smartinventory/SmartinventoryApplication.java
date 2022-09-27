c1c5acb551fdc81bdebf8ba7ccbd1d8fdbf051ff
Hong Yao <hongyao.lim.2021@scis.smu.edu.sg>
2022-09-27 09:25:26 +0800

    added datasource

--- src/main/java/com/smartinventory/SmartinventoryApplication.java
+++ src/main/java/com/smartinventory/SmartinventoryApplication.java
@@ -6,9 +6,8 @@ import java.util.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication //(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class SmartinventoryApplication {
 
 	public static void main(String[] args) {
