29c5fb7e52d401012b28de91bfa3369cb6102d98
ynseah.2021 <ynseah.2021@smu.edu.sg>
2022-09-29 17:56:44 +0800

    completed UserServiceTest

--- src/test/java/com/smartinventory/UserServiceTest.java
+++ src/test/java/com/smartinventory/UserServiceTest.java
@@ -1,24 +1,57 @@
 package com.smartinventory;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.MissingFormatArgumentException;
+import java.util.Optional;
+
+import com.smartinventory.exceptions.user.UserEmailNotFoundException;
+import com.smartinventory.exceptions.user.UserEmailTakenException;
+import com.smartinventory.security.token.ConfirmationTokenService;
+import com.smartinventory.user.User;
 import com.smartinventory.user.UserRepository;
 import com.smartinventory.user.UserService;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
     @Mock
     private UserRepository users;
 
+    @Mock
+    private ConfirmationTokenService tokenService;
+
+    @Mock
+    private BCryptPasswordEncoder encoder;
+
     @InjectMocks
     private UserService userService;
 
     @Test
     void addUser_NewUsername_SavedBook() {
-        
+
+        User user = new User("a@gmail.com", "user", "password");
+
+        Optional<User> userOptional = Optional.empty();
+        when(users.findByUsername(any(String.class))).thenReturn(userOptional);
+        when(users.findByEmailIgnoreCase(any(String.class))).thenReturn(userOptional);
+
+        String regUser = userService.registerUser(user);
+        assertNotNull(regUser);
+
+        verify(users).findByUsername(user.getUsername());
+        verify(users).findByEmailIgnoreCase(user.getEmail());
     }
 }
