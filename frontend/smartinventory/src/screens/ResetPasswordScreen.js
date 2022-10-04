ad008b8140ffc49db927877e7b561318dc71c49d
Ansella <anlimantara.2021@scis.smu.edu.sg>
2022-10-04 15:09:54 +0800

    new screen

---
+++ frontend/smartinventory/src/screens/ResetPasswordScreen.js
@@ -0,0 +1,11 @@
+//import React, { useState } from "react";
+
+function resetPasswordScreen() {
+    return (
+      <div className="hello">
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+  
+  export default resetPasswordScreen;
