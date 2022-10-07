e68576a327df2d010b7f9469f900e7d24fd6f59e
Hong Yao <hongyao.lim.2021@scis.smu.edu.sg>
2022-10-07 12:45:03 +0800

    Saving

--- backend/src/main/java/com/smartinventory/auth/AuthService.java
+++ backend/src/main/java/com/smartinventory/auth/AuthService.java
@@ -50,7 +50,7 @@ public class AuthService {
                 "Link will expire in 15 minutes.%n", reqUsername, confirmationLink);
 
         // Send email
-        // emailSender.send(reqEmail, emailBody, "SmartInventory: Confirm Your Email");
+        emailSender.send(reqEmail, emailBody, "SmartInventory: Confirm Your Email");
         return token;
     }
 
